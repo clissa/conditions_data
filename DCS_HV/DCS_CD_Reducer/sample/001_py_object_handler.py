a01e454303aab5501e7460eef0b728c518575293
clissa <luca.clissa2@unibo.it>
2018-11-23 16:36:11 +0100

    Added empty modules scripts into sample directory within develop branch.

---
+++ DCS_HV/DCS_CD_Reducer/sample/001_py_object_handler.py
@@ -0,0 +1,43 @@
+#!/usr/bin/env python3
+# -*- coding: utf-8 -*-
+"""
+Copyright 2018 Luca Clissa
+
+This file is part of DCS_CD_Reducer.
+
+DCS_CD_Reducer is free software: you can redistribute it and/or modify
+it under the terms of the GNU General Public License as published by
+the Free Software Foundation, either version 3 of the License, or
+(at your option) any later version.
+
+DCS_CD_Reducer is distributed in the hope that it will be useful,
+but WITHOUT ANY WARRANTY; without even the implied warranty of
+MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+GNU General Public License for more details.
+
+You should have received a copy of the GNU General Public License
+along with DCS_CD_Reducer.  If not, see <https://www.gnu.org/licenses/>.
+
+
+
+
+Created on Mon Nov 19 17:12:02 2018
+
+@author: Luca Clissa
+"""
+
+"""
+========================
+Python Objects Handler
+========================
+
+==================== =========================================================
+Utility functions
+==============================================================================
+save_object          Save object from environment to local server.
+get_object           Load pickle file from local server into environment.
+==================== =========================================================
+
+"""
+
+        
---
+++ DCS_HV/DCS_CD_Reducer/sample/002_sqldb_handler.py
@@ -0,0 +1,41 @@
+#!/usr/bin/env python3
+# -*- coding: utf-8 -*-
+"""
+
+Copyright 2018 Luca Clissa
+
+This file is part of DCS_CD_Reducer.
+
+DCS_CD_Reducer is free software: you can redistribute it and/or modify
+it under the terms of the GNU General Public License as published by
+the Free Software Foundation, either version 3 of the License, or
+(at your option) any later version.
+
+DCS_CD_Reducer is distributed in the hope that it will be useful,
+but WITHOUT ANY WARRANTY; without even the implied warranty of
+MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+GNU General Public License for more details.
+
+You should have received a copy of the GNU General Public License
+along with DCS_CD_Reducer.  If not, see <https://www.gnu.org/licenses/>.
+
+
+
+
+Created on Mon Nov 19 17:32:51 2018
+
+@author: Luca Clissa
+"""
+"""
+========================
+SQL DataBase Handler
+========================
+
+==================== =========================================================
+Utility functions
+==============================================================================
+create_connection    Connect to a SQLite Database.
+select_channel       Extract channels and return data and knots dictionaries.
+==================== =========================================================
+
+"""
